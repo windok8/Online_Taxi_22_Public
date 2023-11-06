8f0228ecbfe20fa5924ccea00c9ac6b6030a05d7
windok8 <windokcn@gmail.com>
2023-11-06 09:18:27 +0800

    测试 Token 的 Controller 方法

--- api-passenger/src/main/java/com/wdk/apipassenger/controller/TestController.java
+++ api-passenger/src/main/java/com/wdk/apipassenger/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.wdk.apipassenger.controller;
 
+import com.wdk.internalcommon.dto.ResponseResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Struct;
 
 /**
  * @author : Windok
@@ -17,4 +21,25 @@ public class TestController {
         return "test api passenger";
     }
 
+    /**
+     * @Author: Windok
+     * @Description:    需要有token才能访问的接口
+     * @Date: 2023-10-27
+     * @return ResponseResult
+     **/
+    @GetMapping("/authTest")
+    public ResponseResult authTest(){
+        return ResponseResult.success("authTest");
+    }
+
+    /**
+     * @Author: Windok
+     * @Description:    不需要token就能访问的接口
+     * @Date: 2023-10-27
+     * @return ResponseResult
+     **/
+    @GetMapping("/noauthTest")
+    public ResponseResult noauthTest(){
+        return ResponseResult.success("noauthTest");
+    }
 }
---
+++ internal-common/src/main/java/com/wdk/internalcommon/util/JwtUtils.java
@@ -0,0 +1,16 @@
+package com.wdk.internalcommon.util;
+
+/**
+ * @author : Windok
+ * @date: 2023-11-05
+ * @Description:
+ * @version: 1.0
+ */
+public class JwtUtils {
+
+    //  生成token
+    public static String getToken() {
+        return null;
+    }
+
+}
