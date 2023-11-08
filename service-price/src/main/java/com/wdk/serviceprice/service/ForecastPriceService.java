ef29e06c1750d25fd57b942074bf0edb541d6197
windok8 <windokcn@gmail.com>
2023-11-08 16:37:46 +0800

    service-price 计价计算完成

--- service-price/src/main/java/com/wdk/serviceprice/service/ForecastPriceService.java
+++ service-price/src/main/java/com/wdk/serviceprice/service/ForecastPriceService.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,7 +50,6 @@ public class ForecastPriceService {
         log.info("目的地经度: " + destLatitude);
         log.info("目的地纬度: " + destLongitude);
 
-        log.info("调用地图服务，查询距离和时长");
         ForecastPriceDTO forecastPriceDTO = new ForecastPriceDTO();
         forecastPriceDTO.setDepLatitude(depLatitude);
         forecastPriceDTO.setDepLongitude(depLongitude);
@@ -61,11 +61,10 @@ public class ForecastPriceService {
         log.info("距离: " + distance);
         log.info("时长: " + duration);
 
-        log.info("读取计价规则");
 
         Map<String, Object> queryMap = new HashMap<>();
-        queryMap.put("city_code","110000");
-        queryMap.put("vehicle_type","1");
+        queryMap.put("city_code", "110000");
+        queryMap.put("vehicle_type", "1");
         List<PriceRule> priceRules = priceRuleMappper.selectByMap(queryMap);
         if (Objects.isNull(priceRules) || priceRules.size() == 0) {
             log.info("没有找到计价规则");
@@ -76,9 +75,62 @@ public class ForecastPriceService {
 
 
         ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
-        forecastPriceResponse.setPrice(13.14);
+        forecastPriceResponse.setPrice(getPrice(distance, duration, priceRules.get(0)));
 
-        return ResponseResult.success();
+        return ResponseResult.success(forecastPriceResponse);
     }
+
+    /**
+     * @param distance  距离
+     * @param duration  时长
+     * @param priceRule 计价规则
+     * @return Double
+     * @Author: Windok
+     * @Description: 根据距离和时长计算价格
+     **/
+    private double getPrice(Integer distance, Integer duration, PriceRule priceRule) {
+        //  BigDecimal
+        BigDecimal price = new BigDecimal(0);
+        //  起步价
+        Double startFare = priceRule.getStartFare();
+        BigDecimal startFareBigDecimal = new BigDecimal(startFare);
+        price = price.add(startFareBigDecimal);
+        //  总里程 m
+        BigDecimal distanceBigDecimal = new BigDecimal(distance);
+        //  总里程 km
+        BigDecimal distanceMileDecimal = distanceBigDecimal.divide(new BigDecimal(1000), 2, BigDecimal.ROUND_HALF_UP);
+        //  起步里程
+        Integer startMile = priceRule.getStartMile();
+        BigDecimal startMileBigDecimal = new BigDecimal(startMile);
+        //  超出起步里程的里程
+        double distanceSubtract = distanceMileDecimal.subtract(startMileBigDecimal).doubleValue();
+        //  最终收费里程数
+        Double mile = distanceSubtract < 0 ? 0 : distanceSubtract;
+        BigDecimal mileBigDecimal = new BigDecimal(mile);
+        //  计程单价
+        Double unitPricePerMile = priceRule.getUnitPricePerMile();
+        BigDecimal unitPricePerMileBigDecimal = new BigDecimal(unitPricePerMile);
+        //  计程单价 * 最终收费里程数
+        BigDecimal maileFare = mileBigDecimal.multiply(unitPricePerMileBigDecimal).setScale(2, BigDecimal.ROUND_HALF_UP);
+        price = price.add(maileFare);
+
+        //  时长费
+        BigDecimal time = new BigDecimal(duration);
+        //  时长分钟数
+        BigDecimal timeDecimal = time.divide(new BigDecimal(60), 2, BigDecimal.ROUND_HALF_UP);
+        //  计时单价
+        Double unitPricePerMinute = priceRule.getUnitPricePerMinute();
+        BigDecimal unitPricePerMinuteBigDecimal = new BigDecimal(unitPricePerMinute);
+        //  计时单价 * 时长分钟数
+        BigDecimal timeFare = timeDecimal.multiply(unitPricePerMinuteBigDecimal).setScale(2, BigDecimal.ROUND_HALF_UP);
+        //  总价格
+        price = price.add(timeFare);
+
+
+        return price.doubleValue();
+    }
+
+
+
 
 }
